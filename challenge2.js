c8f980e54e64dd6c48963e7b8b52f7b60d7f4049
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-09 13:22:46 -0600

    Add function

---
+++ challenge2.js
@@ -0,0 +1,21 @@
+// Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+//need strings to print out with lowercase letters only
+//array, strings, loop?
+//will this need a loop  for an array
+//first start with array + strings, loop, then console.log
+
+var words = ["Love", "MOUSE", "NotEs", "fork"];
+// function printLowerCase(words) {
+    for (var i = 0; i < words.length; i++) {
+        console.log(words[i].toLowerCase());
+    }
+// }
+// printLowerCase();
+
+// var words =[ "Love", "MOUSE", "NotEs", "fork"];
+// function printLowerCase(words) 
+// for (i = 0; i < words.length; i++) {
+//     var printLowercase = words[i].toLowerCase();
+//     console.log(lowercaseWord);
+  
