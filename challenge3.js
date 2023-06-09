3c2247904cbbc8e7324dd17cc1116f7773a94198
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-09 13:45:45 -0600

    Initial commit

---
+++ challenge3.js
@@ -0,0 +1,15 @@
+//Write a method or function that removes all instances of the letter s in a string. The method or function should accept a string as an argument and return the same string with every instance of the letter s removed.
+//need to write function that removes letter s in a string, need string to fitr in argument 
+//function, string, parameter, argument, variable, return, console.log to check
+// start with variable, function, include a parameter, set a return value and variable, check this with console.log
+// replace all needed?
+
+
+var string = "this sentence is complete"
+function removeS(string) {
+    return string.replaceAll("s", "")
+}
+console.log(removeS(string));
+// console.log(removeS("kasandra"))
+    
+    
